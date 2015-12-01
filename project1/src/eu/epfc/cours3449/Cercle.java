1122594542cc63f4d2fcfb0794bc9eafd0aa7376
localwsp <localwsp@PC8832.domepfc.net>
2015-12-01 21:48:46 +0100

    Testcercle number of instances

--- project1/src/eu/epfc/cours3449/Cercle.java
+++ project1/src/eu/epfc/cours3449/Cercle.java
@@ -4,11 +4,23 @@ import java.util.*;
 
 public class Cercle {
     static int nbInstances=0; // cela permet de faire appel à cette variable partout et en transversal
-    double rayon;
+    static double rayon;
 
     public Cercle(double rayon) {
         this.rayon = rayon;
         nbInstances++;
+    }
+
+    public static int getNbInstances() {
+        return nbInstances;
+    }
+
+    public static double getRayon() {
+        return rayon;
+    }
+
+    public static void setRayon(double rayon) {
+        Cercle.rayon = rayon;
     }
     
     public double getArea() {
