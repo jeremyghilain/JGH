4f042cdc16f873811a00f8fa5ee72fd10ce9b5d2
localwsp <localwsp@PC8832.domepfc.net>
2015-12-01 20:43:14 +0100

    Random test

--- project1/src/eu/epfc/cours3449/project1.java
+++ project1/src/eu/epfc/cours3449/project1.java
@@ -1,22 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package eu.epfc.cours3449;
 
-/**
- *
- * @author localwsp
- */
+import java.util.*;
+
 public class project1 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hello Git, this is a test");
+        
+        generaterandomint(10, 1000);
+        System.out.println("");
+        
+        generaterandomint(10, 1000);
+    }
+
+    private static void generaterandomint(int n, int seed) {
+        Random r2= new Random();
+        for (int j=0;j<n;j++) {
+            System.out.println(r2.nextInt(seed));
+        }
     }
     
 }
