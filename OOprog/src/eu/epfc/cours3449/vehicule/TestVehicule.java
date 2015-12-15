bc195b901e12b7133c02e5025098051574b8244f
localwsp <localwsp@PC8827.domepfc.net>
2015-12-15 19:26:59 +0100

    encapsulation

--- OOprog/src/eu/epfc/cours3449/vehicule/TestVehicule.java
+++ OOprog/src/eu/epfc/cours3449/vehicule/TestVehicule.java
@@ -3,10 +3,10 @@ package eu.epfc.cours3449.vehicule;
 
 public class TestVehicule {
     public static void main(String[] args) {
-        Vehicule[] vs= new Vehicule[4];
+        Vehicule[] vs= new Vehicule[6];
         
         vs[0]=new Vehicule();
-        vs[0].setAge(1);
+        vs[0].setAge(1,1);
         
         vs[1]=new Bateau();
         vs[1].setAge(12);
@@ -16,6 +16,12 @@ public class TestVehicule {
         
         vs[3]=new Bateau();
         vs[3].setAge(20);
+        
+        vs[4]=new Vehicule();
+        vs[4].setAge(400,0);
+        
+        vs[5]=new Vehicule();
+        vs[5].setAge(100,1);
         
         for(int i=0; i<vs.length; i++) {
             vs[i].start();
--- OOprog/src/eu/epfc/cours3449/vehicule/Vehicule.java
+++ OOprog/src/eu/epfc/cours3449/vehicule/Vehicule.java
@@ -6,19 +6,38 @@ Vehicule est une classe mère ou une super classe
 package eu.epfc.cours3449.vehicule;
 
 public class Vehicule {
-    private int age;
+    private double age; // en jours
     
     public void Vehicule(int age){
         this.age=age;
     }
     
-    public int getAge(){
+    public double getAge(){
+        return age/365;
+    }
+    
+    public double getAge(int unit){
+        switch (unit){
+            case 0: return age; // en jours
+            case 1: getAge(); // en années
+            default: System.out.println("Valeur de code hors limite "+unit);
+        }
         return age;
     }
     
     public void setAge(int age){
         if(age>=0 && age<=5000){
-            this.age=age;
+            this.age=age*365;
+        }
+    }
+
+    public void setAge(int age, int unit){
+        switch (unit){
+            case 0: // en jours
+                if(age>=0 && age<=5000*365){this.age=age;}
+                break;
+            case 1: setAge(age); break; // en années
+            default: System.out.println("Valeur de code hors limite "+unit);
         }
     }
     
