31d2b1f9acf00a9ac187ceb2cfdbe9702c25330a
localwsp <localwsp@PC8827.domepfc.net>
2015-12-15 20:19:23 +0100

    circle in pane

--- OOprog/src/eu/epfc/cours3449/FXGui/CircleInPane.java
+++ OOprog/src/eu/epfc/cours3449/FXGui/CircleInPane.java
@@ -11,7 +11,9 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
+import javafx.scene.paint.Color;
 
 public class CircleInPane extends Application {
     
@@ -22,6 +24,23 @@ public class CircleInPane extends Application {
 
     @Override
     public void start(Stage stage) {
+        Circle c=new Circle();
+        c.setRadius(80);
+        c.setCenterX(150);
+        c.setCenterY(150);
+        c.setStroke(Color.BLUE);
+        c.setStrokeWidth(7);
+        c.setFill(Color.YELLOW);
+        Pane p=new Pane();
+        p.getChildren().add(c);
+        Scene sc = new Scene(p,300,300);
+        Stage st = new Stage ();
+        st.setX(500);
+        st.setY(400);
+        st.setScene(sc);
+        st.setTitle("Circle in pane");
+        st.show();
+        
         
     }
 }
